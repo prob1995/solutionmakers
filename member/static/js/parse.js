74a9de57f32f0fe38e10a10082e47498ca260566
prob1995 <prob1995@gmail.com>
2015-09-09 08:29:12 +0000

    sm ver.0.5

--- member/static/js/parse.js
+++ member/static/js/parse.js
@@ -22,7 +22,8 @@ function newAccount(){
   user.signUp(null, {
     success: function(user) {
       // Hooray! Let them use the app now.
-      alert('耶！註冊成功!')
+      alert('耶！註冊成功!');
+      window.location = "../";
     },
     error: function(user, error) {
       // Show the error message somewhere and let the user try again.
